420d9cf5f677858176fe260dbc1c40cc7f8ea9db
Rachael McIlhagga <rachael.mcilhagga@live.co.uk>
2021-02-10 18:44:29 -0500

    fixing api routes

--- models/workout.js
+++ models/workout.js
@@ -1,27 +1,46 @@
-const mongoose = require("mongoose");
-
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//Creating a schema with the fields that we need
+// Creating a new schema with the fields that we need
 const workoutSchema = new Schema({
-  //Creating a date field, mongoose has a built in method for getting the date
-  date: {
-    type: Date,
-    default: Date.now
-  },
-  //Creating the exercise fields an an array to hold all of the exercise proprties I need it to
-  exercise: [{
-    type: { type: String, trim: true, required: "Enter the name of the exercise" },
-    name: { type: String, trim: true, required: "Enter the type of workout" },
-    duration: { type: Number, default: 0, required: "Enter the duration"},
-    weight: { type: Number, default: 0 },
-    reps: { type: Number, default: 0 },
-    sets: { type: Number, default: 0 },
-    distance: { type: Number, default: 0}
-  }]
-
+    // Creating a date field, mongoose accepts "Date" as a type
+    // Mongoose has a built in feature of getting the date
+    // We use Date.now and not Date.now();
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    // Creating the exercises field as an array to hold the actual exercise properties
+    exercises: [{
+        name: {
+            type: String,
+            trim: true
+        },
+        type: {
+            type: String,
+            trim: true
+        },
+        weight: {
+            type: Number,
+            default: 0
+        },
+        sets: {
+            type: Number,
+            default: 0
+        },
+        reps: {
+            type: Number,
+            default: 0
+        },
+        duration: {
+            type: Number,
+            default: 0
+        },
+        distance: {
+            type: Number,
+            default: 0
+        }
+    }],
 });
-
-//Creates the collection/table workout
-const workout = mongoose.model("Workout", workoutSchema);
-
+// Creating our table name (workout) and implementing our schema (workoutSchema)
+const workout = mongoose.model('workout', workoutSchema);
 module.exports = workout;
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,66 +1,49 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
-
-//Creating the route for creating a new workout 
-router.post('/api/workouts', (req, res) => {
-    Workout.create(req.body)
-    .then((data) => {
-        console.log(data)
-        //sending data to front end 
-        res.json(data)
-    })
-    .catch((err) => {
-        res.status(400).json(err);
-    })
-})
-
-//Creating the route for adding exercise to the workout
-router.put('/api/workouts/:id', (req, res) => {
-        Workout.findOneAndUpdate(req.params.id,
-            {
-                $push: 
-                {
-                    exercise: req.body
-                }
-            })
-            .then((data) => {
-                res.json(data)
-            })
-            .catch((err) => {
-                res.status(400).json(err);
-            })
-    })
-    
-
-//Get the sum of the duration of the workout
+//get workouts
 router.get("/api/workouts", (req, res) => {
     Workout.aggregate([{
-        $addFields: { 
-            totalDuration: { $sum: "$exercises.duration" } 
-        },
-    }, ])   
-    .then((data) => {
-        res.json(data)
-    })
-    .catch((err) => {
-        res.status(400).json(err);
-    })
-})
-
-//Get the sum of the duration of workouts in a seven day range. 
+            $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+        }, ])
+        .then((data) => {
+            res.json(data);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+// add exercise
+router.put("/api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body })
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+//create workout
+router.post("/api/workouts", ({ body }, res) => {
+    Workout.create(body)
+        .then((dbWorkout) => {
+            res.json(dbWorkout);
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+// get workouts in range
 router.get("/api/workouts/range", (req, res) => {
     Workout.aggregate([{
-        $addFields: { 
-            totalDuration: { $sum: "$exercises.duration" } },
-    }])
-    .sort({ day: -1 })
-    .limit(7)
-    .then((data) => {
-        res.json(data.reverse());
-    })
-    .catch((err) => {
-        res.status(400).json(err);
-    });
+            $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+        }])
+        .sort({ day: -1 })
+        .limit(7)
+        .then((data) => {
+            res.json(data.reverse());
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
 });
-
 module.exports = router;
